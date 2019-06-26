92bd1e4ec8cd2a5af1b7fe55577b473d81d7cc62
Andrew Sanchez <andrew.sanchez046@gmail.com>
2019-06-26 11:24:16 -0700

    threaded system finished

--- weather_station.py
+++ weather_station.py
@@ -9,8 +9,8 @@ text file that can be imported to Excel/LibreOffice.
 '''
 import sys
 import Adafruit_DHT
-import time
-import RPI.GPIO as GPIO
+from threading import Event
+import RPi.GPIO as GPIO
 
 # Initialize DHT11 sensor
 sensor = Adafruit_DHT.DHT11;
@@ -24,8 +24,17 @@ reset_btn = 26
 stop_btn = 19
 data_pin = 21
 
+# Setup shutdown event
+shutdown = Event()
+
+# Define system variables
+freq = 1.8    #2 seconds
+time_elap = 0
+data_buf = []
+
 # Raspberry Pi Setup
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 # LEDs
 GPIO.setup(reset_led, GPIO.OUT)
 GPIO.setup(stop_led, GPIO.OUT)
@@ -34,62 +43,65 @@ GPIO.setup(less23_led, GPIO.OUT)
 # Buttons
 GPIO.setup(reset_btn, GPIO.IN)
 GPIO.setup(stop_btn, GPIO.IN)
-#attach interrupts to buttons
-GPIO.add_event_detect(reset_btn, GPIO.FALLING, callback=reset_event)
-GPIO.add_event_detect(stop_btn, GPIO.FALLING, callback=stop_event)
-
 
 # Reset button has been pressed reinitialize data and reset time_elap
 def reset_event(channel):
     GPIO.output(reset_led, GPIO.HIGH)
-    data_buf.clear()
-    time_elap = 0
+    global data_buf
+    global time_elap
+    time_elap = -2
+    del data_buf[:]
+    print 'Resetting system ... \t' +  str(data_buf)
     GPIO.output(reset_led, GPIO.LOW)
 
 # Stop button has been pressed stop main loop
 def stop_event(channel):
     GPIO.output(stop_led, GPIO.HIGH)
-    run = False
-    GPIO.output(stop_led, GPIO.LOW)
+    global shutdown
+    shutdown.set() #trigger event shutdown
+    print('Halting system ...\nLast value recorded will be printed but not added to data:')
+
+#attach interrupts to buttons
+GPIO.add_event_detect(reset_btn, GPIO.FALLING, callback=reset_event)
+GPIO.add_event_detect(stop_btn, GPIO.FALLING, callback=stop_event)
+
 
 # sends data to text file that can be imported to Excel/LibreOffice
 def to_file():
     print('sent to file')
 
-# Define system variables
-freq = 2    # 2 seconds
-time_elap = 0
-data_buf = []
-run = True
+# initialize LEDs
+GPIO.output(heat_led, GPIO.LOW)
+GPIO.output(less23_led, GPIO.LOW)
+GPIO.output(reset_led, GPIO.LOW)
+GPIO.output(stop_led, GPIO.LOW)
 
 print('Time(Seconds)\tTemperature(C)\tHumidity(%)')
 # Main loop
-while run:
-    # Aquire Data
+while not shutdown.is_set():
+    # AQUIRE Data
+    # Returns values every 2 seconds due to .5Hz temp sensor
     humid, temp = Adafruit_DHT.read_retry(sensor, data_pin)
 
+    # PROCESS and RECORD data
+    row = str(time_elap) + '\t' + str(temp) + '\t' + str(humid)
+    data_buf.append(row)
+    time_elap += freq
+
+    #DISPLAY data
+    print(row)
     # Change LED states
     if temp > 23:
-        GPIO.ouput(heat_led, GPIO.HIGH)
+        GPIO.output(heat_led, GPIO.HIGH)
         GPIO.output(less23_led, GPIO.LOW)
-    else
-        GPIO.ouput(heat_led, GPIO.LOW)
+    else:
+        GPIO.output(heat_led, GPIO.LOW)
         GPIO.output(less23_led, GPIO.HIGH)
 
-    # Display and record data
-    row = time_elap + '\t' + temp + '\t' + humid
-    data_buf.append(row)
-    print(row)
-
-    time.sleep(2000)  # Wait 2 seconds due to .5Hz temp sensor
-    time_elap += 2
-
 # initialize shutdown sequence
-GPIO.ouput(heat_led, GPIO.LOW)
+GPIO.output(heat_led, GPIO.LOW)
 GPIO.output(less23_led, GPIO.LOW)
-GPIO.ouput(reset_led, GPIO.LOW)
-GPIO.output(stop_led, GPIO.HIGH)
+GPIO.output(reset_led, GPIO.LOW)
 # send data to formatted text file
 to_file()
-GPIO.ouput(stop_led, GPIO.LOW)
 GPIO.cleanup() # end program
