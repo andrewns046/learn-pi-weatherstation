b6b7284527b8d23f4a981f6ddd7a2b1f71b136b5
Andrew Sanchez <andrew.sanchez046@gmail.com>
2019-07-11 09:34:45 -0700

    updated ir project

--- blinkcounter.py
+++ blinkcounter.py
@@ -1,39 +1,46 @@
+# TODO blink one led with polling and the other with edge trigger
+
 class BlinkCounter():
 
-    def __init__(self, led_pin, ir_pin):
-        import RPI.GPIO as GPIO
+    def __init__(self, rled_pin, gled_pin, ir_pin):
+        import RPi.GPIO as GPIO
         import time
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(led_pin, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(rled_pin, GPIO.OUT)
+        GPIO.setup(gled_pin, GPIO.OUT)
         GPIO.setup(ir_pin, GPIO.IN)
 
-        self.led_pin = led_pin
-        self.count = 0
+        self.rled_pin = rled_pin
+        self.ir_pin = ir_pin
+        self.gled_pin = gled_pin
+        
+        GPIO.add_event_detect(ir_pin, GPIO.RISING, callback=self.ris_trigger)
+        
+        self.poll_count = 0
+        self.edge_count = 0
         self.on = True
-
+        
+    def ris_trigger(self, channel):
+        self.edge_count += 1
+        
 
     # blinks led count amount of times
     def poll(self):
-        import RPI.GPIO as GPIO
-        while(self.on):
-            if( GPIO.input(self.ir_pin) ):
-                self.count += 1
-
-            # equivalent to all the timing we will be doing
-            for i in range(100):pass
-
-    def blink(self, blinks):
-        import RPI.GPIO as GPIO
-        for i in range(blinks):
-            GPIO.output(self.led_pin, GPIO.HIGH)
-
-
+        import RPi.GPIO as GPIO
+        import time
+        while (self.on):
+            if( GPIO.input(self.ir_pin) ):  # if signal is high
+                self.poll_count += 1
+                # blink red led
+                GPIO.output(self.rled_pin, GPIO.HIGH)
+                time.sleep(1)
+                GPIO.output(self.rled_pin, GPIO.LOW)
+            
+    # shutdown process
     def shutdown(self):
-        import RPI.GPIO as GPIO
+        import RPi.GPIO as GPIO
         import time
         self.on = False
-        print('Stopping IR Sensor')
-        time.sleep(.5)
-        print('The led should blink %d times' % self.count)
-        self.blink(self.count)
+        print('Stopping IR Sensor ...')
+        print('The red led blinked %d times and the green led blinked %d times' % (self.poll_count, self.edge_count))
         GPIO.cleanup()
