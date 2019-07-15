f3b2e07bf53013124689e9e7ce7e6d8251949231
Andrew Sanchez <andrew.sanchez046@gmail.com>
2019-07-15 09:38:01 -0700

    ready to test marble track

--- blinkcounter.py
+++ blinkcounter.py
@@ -1,46 +1,42 @@
-# TODO blink one led with polling and the other with edge trigger
+import RPi.GPIO as GPIO
+import time
 
 class BlinkCounter():
-
-    def __init__(self, rled_pin, gled_pin, ir_pin):
-        import RPi.GPIO as GPIO
-        import time
+    
+    # pin list refers to the GPIO pins we are using for the photo gates
+    # ex. [21, 16, 6, 12, 24, 17]
+    # pin 21 should be PHOTO GATE #1, pin 16 should be PHOTO GATE #2,
+    # pin 6 should be PHOTO GATE #3, ...
+    def __init__(self, ir_pin):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(rled_pin, GPIO.OUT)
-        GPIO.setup(gled_pin, GPIO.OUT)
         GPIO.setup(ir_pin, GPIO.IN)
-
-        self.rled_pin = rled_pin
         self.ir_pin = ir_pin
-        self.gled_pin = gled_pin
         
+        # self.photo_gates = pin_list
+        
+        '''
+        # setup all photo gates
+        for gate in self.photo_gates:
+            GPIO.setup(gate, GPIO.IN)
+            GPIO.add_event_detect(gate, GPIO.RISING, callback=self.ris_trigger)
+        '''
         GPIO.add_event_detect(ir_pin, GPIO.RISING, callback=self.ris_trigger)
         
-        self.poll_count = 0
         self.edge_count = 0
-        self.on = True
-        
+    
+    # trigger when rising is detected
     def ris_trigger(self, channel):
         self.edge_count += 1
+        print('Yeet !')  # I love Bobby <3
+    
+    # Description:
+    # Step 1: Drop marble into marble track
+    # Step 2: Start timing when marble reaches PHOTO GATE #1
+    def start_exp(self, timeout):pass
         
-
-    # blinks led count amount of times
-    def poll(self):
-        import RPi.GPIO as GPIO
-        import time
-        while (self.on):
-            if( GPIO.input(self.ir_pin) ):  # if signal is high
-                self.poll_count += 1
-                # blink red led
-                GPIO.output(self.rled_pin, GPIO.HIGH)
-                time.sleep(1)
-                GPIO.output(self.rled_pin, GPIO.LOW)
-            
-    # shutdown process
+    
+    # define a shutdown process
     def shutdown(self):
-        import RPi.GPIO as GPIO
-        import time
-        self.on = False
         print('Stopping IR Sensor ...')
-        print('The red led blinked %d times and the green led blinked %d times' % (self.poll_count, self.edge_count))
+        print('Counted %d times' % (self.edge_count))
         GPIO.cleanup()
